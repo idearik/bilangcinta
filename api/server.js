7e2d3869df02a63431b494210fd11398e1d90514
Ari Kurniawan <39238418+idearik@users.noreply.github.com>
2024-06-28 22:45:07 +0800

    Vercel update

--- api/server.js
+++ api/server.js
@@ -13,8 +13,8 @@ db.serialize(() => {
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 // Serve the main page
@@ -55,7 +55,4 @@ app.get('/search', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
